27ae965593865e3bb603ebb85298cf4021cf1be3
andrewlrogers <rogers.andrew@gmail.com>
2017-02-12 20:52:00 -0800

    was able to upload file using code from stackexchange

--- drop.py
+++ drop.py
@@ -4,15 +4,15 @@ import re
 
 # the source file
 folder = pathlib.Path(".")    # located in this folder
-filename = "test.txt"         # file name
+filename = "questions.csv"         # file name
 filepath = folder / filename  # path object, defining the file
 
 # target location in Dropbox
-target = "/Temp/"              # the target folder
+target = "/"              # the target folder
 targetfile = target + filename   # the target path and file name
 
 # Create a dropbox object using an API v2 key
-d = dropbox.Dropbox(your_api_access_token)
+d = dropbox.Dropbox("I4gEG90KcJAAAAAAAAACOLjenKg2dUhaqFKNRYVOgiCnBQ6VvDFA3WUMKaZVJNBJ")
 
 # open the file and upload it
 with filepath.open("rb") as f:
