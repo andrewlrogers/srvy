e42cd4bc9e651349fc10263b0542dbb32901054b
andrewlrogers <rogers.andrew@gmail.com>
2017-02-19 14:29:44 -0800

    added ability to write csv from db

--- write_csv.py
+++ write_csv.py
@@ -2,7 +2,10 @@ import sqlite3
 import csv
 from datetime import datetime
 
-destination_file = 'srvy.csv'
+
+current_date = datetime.now().strftime('%Y-%m-%d')
+
+destination_file = 'srvy' + current_date + '.csv'
 sqlite_file = 'srvy.db'
 table_name = 'responses'
 date_column = 'date'
@@ -10,17 +13,16 @@ time_column = 'time'
 score_column = 'score'
 question_column = 'question'
 
-current_date = datetime.now().strftime('%Y-%m-%d')
 
 conn =  sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 
-survey  = c.execute("SELECT * FROM responses WHERE date LIKE '%2017-02-18%';")
+c.execute("SELECT * FROM responses WHERE date LIKE '%2017-02-18%';")
 
-print(c.fetchall())
+csvWriter = csv.writer(open(destination_file, 'w'))
+rows = c.fetchall()
 
-#for row in rows:
-#    c.execute('SELECT * FROM responses')
-#    csvWriter.writerows(rows)
+for row in rows:
+    csvWriter.writerow(row)
 
