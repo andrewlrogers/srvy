8d55d3b42c52b10a909addab0bafa8771e911c28
Brian Suhr <brian@briansuhr.com>
2017-02-19 21:31:03 -0800

    Save csv files to "export" directory

--- write_csv.py
+++ write_csv.py
@@ -1,11 +1,17 @@
+"""Export all responses from yesterday and save them to a CSV file."""
+
 import sqlite3
 import csv
 from datetime import datetime
+import os
 
 
 current_date = str(datetime.now().strftime('%Y-%m-%d'))
 
-destination_file = 'srvy' + current_date + '.csv'
+export_directory = 'export'
+export_filename = 'srvy' + current_date + '.csv'
+full_export_path = os.path.join(export_directory, export_filename)
+
 sqlite_file = 'srvy.db'
 table_name = 'responses'
 date_column = 'date'
@@ -22,7 +28,7 @@ c = conn.cursor()
 
 c.execute("SELECT * FROM responses WHERE date = ?", (current_date,))
 
-csvWriter = csv.writer(open(destination_file, 'w'))
+csvWriter = csv.writer(open(full_export_path, 'w'))
 rows = c.fetchall()
 
 for row in rows:
