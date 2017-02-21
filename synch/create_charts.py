dd877736f8a15579fa0339ece946d0c3f87d82aa
Brian Suhr <brian@briansuhr.com>
2017-02-20 19:05:18 -0800

    Create list of questinos from yesterday

--- synch/create_charts.py
+++ synch/create_charts.py
@@ -1,13 +1,47 @@
 #!/usr/bin/python
 import leather
+import csv
+import sqlite3
 
-data = [
-    (0, 3),
-    (4, 5),
-    (7, 9),
-    (8, 4)
-]
+with open('../export/test.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = list(reader)[:10]
 
-chart = leather.Chart('Simple pairs')
-chart.add_dots(data)
+    for row in data:
+        row[0] = float(row[0]) if row[0] is not None else None
+
+chart = leather.Chart('Data from CSV reader')
+chart.add_bars(data)
 chart.to_svg('../export/charts/simple_pairs.svg')
+
+
+###########
+from datetime import datetime, timedelta
+import os
+
+today = str(datetime.now().strftime('%Y-%m-%d'))
+yesterday = str((datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'))
+
+export_directory = '../export'
+export_filename = 'srvy' + yesterday + '.csv'
+full_export_path = os.path.join(export_directory, export_filename)
+
+sqlite_file = '../srvy.db'
+table_name = 'responses'
+
+conn =  sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+questions_from_yesterday = []
+
+try:
+    responses = c.execute("SELECT question FROM responses WHERE date = ?", (yesterday,))
+    for response in responses:
+        if response not in questions_from_yesterday:
+            questions_from_yesterday.append(response)
+except:
+    pass
+
+
+print(questions_from_yesterday)
