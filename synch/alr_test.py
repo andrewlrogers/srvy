016b530e523517529addb75f6019086a6bb0c47f
Brian Suhr <brian@briansuhr.com>
2017-03-14 21:18:41 -0700

    Create function to get unique questions by date

--- synch/alr_test.py
+++ synch/alr_test.py
@@ -6,21 +6,36 @@ import string
 
 from bokeh.charts import Bar, Donut, TimeSeries, output_file, show
 
+database_file = "../srvy.db"
+
 #Get time in order
 today = str(datetime.now().strftime('%Y-%m-%d'))
 yesterday = str((datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'))
-fixed_date = '2017-02-26'
 
 def query_by_date(date):
     """Queries the database from a date string in the format YYYY-MM-DD"""
     sql_query = "SELECT * FROM responses where date ='" + str(date) + "'"
     return(sql_query)
 
+def get_unique_questions(date):
+    """Creates a list of all unique questions from a given date"""
+    questions = []
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    sql_query = c.execute(query_by_date(date))
+
+    for response in sql_query:
+        if response[3] not in questions:
+            questions.append(response[3])
+    print(questions)
+    return(questions)
+
+get_unique_questions('2017-02-26')
 #test query to see all output.
 sql_query = query_by_date('2017-02-26')
 
 #set-up our sql cursor
-conn = sqlite3.connect('../srvy.db')
+conn = sqlite3.connect(database_file)
 
 #Setting up our data frame
 # parse_dates='time' gives a KeyError
