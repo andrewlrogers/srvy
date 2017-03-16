77cbc654ea3a06bad134f9286090d6d5b3ed06d1
andrewlrogers <rogers.andrew@gmail.com>
2017-03-15 22:36:24 -0700

    multiple charts in pnada-test

--- synch/panda_test.py
+++ synch/panda_test.py
@@ -8,21 +8,12 @@ from bokeh.charts import Bar, Donut, TimeSeries, output_file, show
 from bokeh.layouts import row
 
 """time variables"""
+fixed_date = '2017-02-26'
 today = str(datetime.now().strftime('%Y-%m-%d'))
 yesterday = str((datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'))
-
-"""initialize variables"""
-
-
-"""Create df from SQL"""
-
 database_file = "../srvy.db"
-conn = sqlite3.connect(database_file)
-sql_query = "SELECT * FROM responses where date ='" + str('2017-02-26') + "'"
-df = pd.read_sql_query(sql_query, conn)
 
-
-def get_unique_questions():
+def get_unique_questions(df):
     """Creates a list of all unique questions from a given date"""
     questions=[]
     for question in df['question']:
@@ -30,22 +21,31 @@ def get_unique_questions():
             questions.append(question)
     return(questions)
 
+def load_dataframe(date):
+    conn = sqlite3.connect(database_file)
+    sql_query = "SELECT * FROM responses where date ='" + str(date) + "'"
+    df = pd.read_sql_query(sql_query, conn)
+    return(df)
 
-def create_charts_for_all_questions():
+def create_charts_for_all_questions(date):
     """Creates charts for all unique questions from a given date"""
-    questions = get_unique_questions()
+    df= load_dataframe(date)
+    questions = get_unique_questions(df)
     chart_group = []
-    count = 1
     for q in questions:
-        bar = Bar(df.loc[df['question'] == q].sort_values(by='score', ascending = False), values = 'score', label = 'question', stack = 'opinion', title = q + ' ' + yesterday, ylabel = 'Number of Responses', agg = 'count', legend= 'bottom_left', palette= ['#693A77', '#8b5c8e', '#ae7ea5', '#d1a1bc',] ,plot_height = 900, plot_width = 600)
-        count += 1
-        #show(bar)
-        chart_group.append(bar)
-    output_file('../export/'+ yesterday + '_digest.html')
+        individual_question_bar = Bar(df.loc[df['question'] == q].sort_values(by='score', ascending = False), values = 'score', label = 'question', stack = 'opinion', title = q + ' ' + yesterday, ylabel = 'Number of Responses', xlabel=' ', agg = 'count', legend= 'bottom_left', palette= ['#693A77', '#8b5c8e', '#ae7ea5', '#d1a1bc',])
+        chart_group.append(individual_question_bar)
+
+    all_questions = Bar(df, values = 'score', label = 'question', stack = 'opinion', title= 'All questions ' + yesterday, ylabel = 'Number of Responses', agg = 'count', legend = 'bottom_left', palette= ['#693A77', '#8b5c8e', '#ae7ea5', '#d1a1bc',])
+    chart_group.append(all_questions)
+
+    donut_chart= Donut(df, values = 'score', label = ['opinion'], title = 'Opinon distibution from ' + yesterday, ylabel = 'Number of Responses', agg = 'count',)
+    chart_group.append(donut_chart)
+    output_file('../export/'+ date + '_digest.html')
     show(row(chart_group))
 
 #The actual chart
 #donut_chart= Donut(df, values = 'score', label = ['opinion'], title = 'Opinon distibution from ' + yesterday, ylabel = 'Number of Responses', agg = 'count',)
 #chron_chart = TimeSeries(df,x = 'time', y = 'opinion', color= 'question', dash='question')
 
-create_charts_for_all_questions()
+create_charts_for_all_questions(fixed_date)
