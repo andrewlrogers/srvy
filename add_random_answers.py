2073942c49cb85664c068412951f2c1f7351679f
Brian Suhr <brian@briansuhr.com>
2017-04-01 20:47:33 -0700

    Print random time based on date range

--- add_random_answers.py
+++ add_random_answers.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import time
 from datetime import datetime, date
+from random import randint
 
 start_date = date(2014, 1, 1)
 end_date = datetime.now()
 date_range = pd.date_range(start_date, end_date)
 
 for date in date_range:
-    print(date)
+    random_hour = randint(10, 17)
+    random_minute = randint(0, 59)
+    random_second = randint(0, 59)
+    new_date = (date.year, date.month, date.day, random_hour, random_minute, random_second)
+    print(new_date)
