26cd550fa16d8a42dec7c72e1e0c0686ae0b0e8c
andrewlrogers <rogers.andrew@gmail.com>
2017-04-26 20:57:00 -0700

    add basic palletes to config.py

--- synch/config.py
+++ synch/config.py
@@ -9,9 +9,11 @@ screen_height=480
 
 #RGB color pallete to show on display during questions
 bg_color = [(105, 58, 119), (162, 173, 0), (125, 154, 170), (86, 90,92)]
+basic_bg_color = [(0,0,0), (50,50,50), (153,153,153)]
 #Pygame font, customize if you know what youre doing.
 font = ("Futura, Helvetica, Arial", 48)
 
 ### F O R   C R E A T E  C H A R T S.py ####
 crocker_purple = ['#693A77', '#8b5c8e', '#ae7ea5', '#d1a1bc']
 crocker_contrast = ['#693A77','#A2AD00', '#565A5C']
+basic = ['#47ED2C', '#A1EF2C', '#F2E72D', '#F68E2E', '#F9332F']
