74ef28bcc48750562ef7c2198a202bef5ae17c3d
Brian Suhr <brian@briansuhr.com>
2017-07-15 14:59:47 -0700

    Reformat code

--- synch/pull_questions.py
+++ synch/pull_questions.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
 """ Downloads questions from dropbox to be integrated into srvy """
-""" Uploads charts and csv's"""
 
 import dropbox
 import pathlib
-import re
 from datetime import datetime, timedelta
 from configparser import ConfigParser
 
@@ -19,15 +17,16 @@ dropbox_token = parser.get('dropbox', 'token')
 today = str(datetime.now().strftime('%Y-%m-%d'))
 yesterday = str((datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d'))
 
-#Creates a dropbox object
+# Creates a dropbox object
 dbx = dropbox.Dropbox(dropbox_token)
 
 """ D O W N L O A D I N G _ FUNCTIONS """
 
+
 def download_questions():
     """downloads csv from dropbox and overwrites locally"""
-    local = '/home/pi/srvy/synch/questions.csv'#The folder/file locally that we want the files to download to.
-    with open (local, 'wb') as f:
+    local = '/home/pi/srvy/synch/questions.csv'  # The folder/file locally that we want the files to download to.
+    with open(local, 'wb') as f:
         metadata, res = dbx.files_download('/questions.csv')
         f.write(res.content)
 
@@ -35,11 +34,12 @@ def download_questions():
 """ U P L O A D I N G _ FUNCTIONS"""
 
 # the source files to upload
-chart_to_upload= yesterday + ".html"
+chart_to_upload = yesterday + ".html"
 csv_to_upload = yesterday + ".csv"
 files_to_upload = (chart_to_upload, csv_to_upload)
 
-def foopa(files_to_upload): #need to change this function name
+
+def foopa(files_to_upload):  # need to change this function name
     for f in files_to_upload:
         try:
             filepath = pathlib.Path("/home/pi/srvy/export/" + f)
@@ -66,4 +66,4 @@ def upload_files(filepath, filename):
 """ M A I N """
 download_questions()
 foopa(files_to_upload)
-#upload_files(pathlib.Path("/home/pi/srvy/export/srvy2017-02-18.csv"), "srvy2017-02-18.csv")
+# upload_files(pathlib.Path("/home/pi/srvy/export/srvy2017-02-18.csv"), "srvy2017-02-18.csv")
