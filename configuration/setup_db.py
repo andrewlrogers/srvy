c00c93be231fe61a123e6e1520558ce63082e8ea
andrewlrogers <rogers.andrew@gmail.com>
2017-11-27 21:37:13 -0800

    create table within db if it does not already exist

--- configuration/setup_db.py
+++ configuration/setup_db.py
@@ -10,5 +10,25 @@ def create_conection(db_file):
     finally:
         conn.close()
 
+def create_table(db_file,create_table_sql):
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close
 
-create_conection("../archive/srvy_failure.db")
+def main():
+    database = "../archive/srvy.db"
+    create_conection(database)
+    create_srvy_table = """ CREATE TABLE IF NOT EXISTS responses (response_key INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    pythonDateTime TEXT NOT NULL,
+                    unixTime REAL NOT NULL,
+                    question TEXT NOT NULL,
+                    opinion INTEGER NOT NULL
+                    );"""
+    create_table(database, create_srvy_table)
+
+main()
